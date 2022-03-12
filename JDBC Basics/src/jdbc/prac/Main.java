d8e24b96c1dd2a239ba099954675dba8b02c48b0
shounak1997 <shounak.gh7@gmail.com>
2022-03-12 13:14:31 +0530

    jdbc basics

---
+++ JDBC Basics/src/jdbc/prac/Main.java
@@ -0,0 +1,18 @@
+package jdbc.prac;
+
+public class Main {
+/*1. Load the driver in memory.
+	Oracle thin driver (Type 4)
+	
+	
+	
+	
+	
+//2.Establish connection with JDBC.
+//3.Create a statement and execute it.
+//4.Handle the exception.
+//5.Close the connection. */
+	public Main() {
+		
+	}
+}
